fbec99c8257fd8dc4ed30ccf24719d15f25488da
itzik-savaia <itziksavaia@gmail.com>
2022-06-23 11:13:45 +0300

    react-native-redux

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { applyMiddleware, configureStore, createStore } from '@reduxjs/toolkit'
 import logger from 'redux-logger'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers/rootReducer'
 
+const middleware = [logger, thunk];
 
-const store = configureStore({
-  reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
-})
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleware)));
+
+
+
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
